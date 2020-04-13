158b27744bb16b45d9c496dc5162e4c25d32a44d
Daniel J Wilcox <danieljwilcox@gmail.com>
2020-04-13 01:17:21 +0100

    config.mk

--- config.mk
+++ config.mk
@@ -7,8 +7,8 @@ VERSION = 0.8.2
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/local/include
+X11LIB = /usr/local/lib
 
 PKG_CONFIG = pkg-config
 
